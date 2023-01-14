c48a3ba886913bbfa6c561bdf7a0edd23456717f
adutlersaar <adutler.saar@gmail.com>
2023-01-14 22:59:05 +0200

    50 epochs

--- train_model.py
+++ train_model.py
@@ -37,7 +37,7 @@ def train(train_df, test_df, output_dir='bert-fine-tuned', model_init=model_init
         learning_rate=2e-5,
         per_device_train_batch_size=32,
         per_device_eval_batch_size=32,
-        num_train_epochs=5,
+        num_train_epochs=50,
         weight_decay=0.01
     )
     trainer = Trainer(
