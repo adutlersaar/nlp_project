dbe2cd9b09c05e2541a7103893ba593d114d7c64
adutlersaar <adutler.saar@gmail.com>
2023-01-17 00:23:55 +0200

    fix

--- augment_dataset.py
+++ augment_dataset.py
@@ -7,7 +7,7 @@ tqdm.pandas()
 
 
 def augment_dataset(paraphraser_name, data_dir='', num_samples=2, **kwargs):
-    train_df = pd.read_csv(Path(data_dir, 'train.csv')).iloc[:3]
+    train_df = pd.read_csv(Path(data_dir, 'train.csv'))
     aug_train_df = train_df['text'].progress_map(lambda x: apply_paraphrasing(x, paraphraser_name, num_samples=num_samples)).explode().reset_index()
     aug_train_df = aug_train_df.join(train_df[['label']], on='index')[['text', 'label', 'index']].reset_index()
     aug_train_df.to_csv(Path(data_dir, f'{paraphraser_name}_aug_train.csv'))
