8fa16ba2b0925920e64ff4cf93fcc5637a8a9f7d
adutlersaar <adutler.saar@gmail.com>
2023-01-16 09:05:12 +0200

    clean nan from t5 data (1 found)

--- load_data.py
+++ load_data.py
@@ -10,6 +10,6 @@ def load_datasets(data_dir='data', with_bart_aug=False, with_t5_aug=False):
     if with_t5_aug:
         train_csvs.append('t5_aug_train.csv')
     train_df = pd.concat([pd.read_csv(Path(data_dir, f_name))[['text', 'label']] for f_name in train_csvs]).reset_index()
-    shuffled_train_df = train_df.sample(frac=1).reset_index()
+    cleaned_shuffled_train_df = train_df[~train_df.text.isna()].sample(frac=1).reset_index()
     test_df = pd.read_csv(Path(data_dir, 'test.csv'))
-    return shuffled_train_df, test_df
+    return cleaned_shuffled_train_df, test_df
